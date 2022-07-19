6c5afbc8fda630ee00f21bc475e0081088fe71fc
Pavel1095 <9153220501@mail.ru>
2022-07-20 00:38:07 +0300

    задание 3 выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
-        int clientOs = 1;
-        if (clientOs == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установитте версию приложения для Android по ссыылке");
-        }
+        // Задание 3\
+        int year = 2020;
+
+        boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+        if (isLeapYear)
+        {
+            System.out.println(year + " is a leap year.");
+        } else
+            System.out.println(year + " is not a leap year.");
     }
 }
