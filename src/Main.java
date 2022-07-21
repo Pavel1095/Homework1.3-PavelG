a7a7acc3e704302d7100715d70b4294b191f8035
Pavel1095 <9153220501@mail.ru>
2022-07-21 06:40:12 +0300

    задание 4 выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
-        int clientOs = 1;
-        if (clientOs == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установитте версию приложения для Android по ссыылке");
-        }
+        // Задание 4
+        int deliveryDictance = 95;
+        if (deliveryDictance <= 20) {
+            System.out.println("Доставка карты займет 1 день");
+        } else if (deliveryDictance >20 && deliveryDictance <= 60) {
+                System.out.println("Доставка карты займет 2 дня");
+        } else if (deliveryDictance > 60 && deliveryDictance <= 100)
+            System.out.println("доставка карты зймет 3 дня");
     }
 }
